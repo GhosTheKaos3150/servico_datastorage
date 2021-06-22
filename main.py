ddd922cb39b53455e92f67f5734132be8499fda9
Uriel Anderson <uriel_anderson@hotmail.com>
2021-06-22 15:08:12 -0300

    Adição de "get_data"

--- main.py
+++ main.py
@@ -27,76 +27,6 @@ app = Flask(__name__)
 # (...)]
 
 
-# @app.route('/add_data', methods=['POST'])
-# def add_data():
-#     # Treating Data
-#     data = request.json
-#
-#     for value in data:
-#         obj_id = value['deviceId']
-#         obj_data = value['value']
-#         obj_date_att = dtt.strptime(value['when'], '%Y-%m-%dT%H:%M:%S.%fZ').date()
-#
-#         network = obj_data['network']
-#
-#         os.chdir('..')
-#         if not 'data' in os.listdir(path.abspath(path.curdir)):
-#             os.mkdir('data')
-#
-#         os.chdir('data')
-#
-#         if f'{network}' in os.listdir(path.abspath(path.curdir)):
-#             os.chdir(f'{network}')
-#         else:
-#             os.mkdir(f'{network}')
-#             os.chdir(f'{network}')
-#
-#         if f'{obj_id}' in os.listdir(path.abspath(path.curdir)):
-#             os.chdir(f'{obj_id}')
-#         else:
-#             os.mkdir(f'{obj_id}')
-#             os.chdir(f'{obj_id}')
-#
-#         if f'{obj_date_att}' in os.listdir(path.abspath(path.curdir)):
-#             os.chdir(f'{obj_date_att}')
-#         else:
-#             os.mkdir(f'{obj_date_att}')
-#             os.chdir(f'{obj_date_att}')
-#
-#         info = {
-#             'id': [obj_id],
-#             'network': [network],
-#             'date': [dtt.now().strftime('%d de %b de %Y, ás %H:%M:%S')]
-#         }
-#         info = pd.DataFrame(data=info)
-#
-#         save_data = obj_data['value']
-#         save_data = {
-#             'x': [save_data['x']],
-#             'y': [save_data['y']],
-#             'z': [save_data['z']],
-#         }
-#
-#         save_data = pd.DataFrame(data=save_data, columns=['x', 'y', 'z'])
-#         save_data['id'] = save_data.index
-#         save_data['date'] = obj_data['when']
-#         save_data['id_obj'] = obj_id
-#
-#         if 'data.csv' in os.listdir(path.abspath(path.curdir)):
-#             prev_data = pd.read_csv(path.abspath(path.curdir) + '/data.csv')
-#             save_data = pd.concat([prev_data, save_data], join='inner', ignore_index=True)
-#
-#         save_data.to_csv(path.abspath(path.curdir) + '/data.csv')
-#         info.to_csv(path.abspath(path.curdir) + '/info.csv')
-#
-#         os.chdir('../../../../servico_datastorage')
-#
-#     return {
-#         'response': 'OK',
-#         'status': 200
-#     }
-
-
 @app.route('/add_data', methods=['POST'])
 def add_data():
 
@@ -168,75 +98,30 @@ def add_data():
     }
 
 
-@app.route('/repair', methods=['GET'])
-def repair_data():
-    # MongoDB - Formato do Documento
-    #
-    # {
-    #   "network": <network>,
-    #   "last_update": <att>,
-    #   "data": [{
-    #       "id": <id>,
-    #       "x": 0,
-    #       "y": 0,
-    #       "z": 0,
-    #       "date": <date>
-    #    }, (...) ]
-    #   }, (...) ]
-    # }
+@app.route('/get/env=<env>&id=<_id>&date=<date>')
+def get_data(env, _id, date):
 
     client = pymongo.MongoClient('mongodb://0.0.0.0:27017')
     database = client['viasoluti-database']
     col = database['data']
 
-    doc = col.find_one({"network": 'casa'})
+    doc = col.find_one({
+        "network": env
+    })
 
-    for value in doc['data']:
-        print(type(value[0]))
+    info = pd.DataFrame(doc['objects'])
+    info = info.loc[info['id'] == _id]
+    info = info['dates'].tolist()[0]
+
+    data_by_date = pd.DataFrame(info)
+    data_by_date = data_by_date.loc[data_by_date['date'] == date]
+    data = data_by_date['data'].tolist()[0]
 
     return {
-        'result': 'OK',
-        'status': 200
+        "responde": "OK",
+        "status": 200,
+        "data": data
     }
-
-
-@app.route('/get/id=<_id>&date=<date>')
-def get_data(_id, date):
-
-    os.chdir('../data')
-    if _id in os.listdir(path.abspath(path.curdir)):
-        os.chdir(path.abspath(path.curdir)+f'/{_id}')
-
-        if date in os.listdir(path.abspath(path.curdir)):
-            os.chdir(path.abspath(path.curdir)+f'/{date}')
-
-            data = pd.read_csv(path.abspath(path.curdir)+f'/data.csv')
-            info = pd.read_csv(path.abspath(path.curdir)+f'/info.csv')
-
-            obj = {
-                'imageType': info['imageType'],
-                'imageName': info['imageName'],
-                'image': info['image'],
-                'anchor': info['anchor'],
-                'dados': data.to_dict()
-            }
-
-            return {
-                'response': obj,
-                'status': 200,
-            }
-
-        else:
-            return {
-                'response': 'INTERNAL SERVER ERROR',
-                'status': 500
-            }
-
-    else:
-        return {
-            'response': 'INTERNAL SERVER ERROR',
-            'status': 500
-        }
 
 
 if __name__ == '__main__':
