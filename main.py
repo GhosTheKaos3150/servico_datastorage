9c1d02895e2c659178138546cd045fe59711562f
Uriel Anderson <uriel_anderson@hotmail.com>
2021-06-24 19:21:35 -0300

    Liberando CORS

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from datetime import datetime as dtt, date as dt
 
 import pandas as pd
 from flask import Flask, request, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # [
 # {
